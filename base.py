63760f5a474eb44dbac4fd26c4085af77d56da6a
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2014-02-09 14:39:23 -0800

    [views] add vote method

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
 from adminviews import GalleryUserView, GalleryPresentationView
@@ -27,9 +27,13 @@ admin.add_view(GalleryPresentationView(db.session))
 def home():
 	return render_template("home.html", presentations=Presentation.query.all())
 
+@app.route('/vote/<int:id_presentation>/<int:id_user>', methods=['POST'])
+def vote(id_presentation, id_user):
+	vote = Vote(id_presentation, id_user)
+	db.session.add(vote)
+	db.session.commit()
+	resp = Response({}, status=200, mimetype='application/json')
+	return resp
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
-
-# <iframe src="http://prezi.com/embed/wp41jvbp-gdv/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;features=undefined&amp;disabled_features=undefined" width="550" height="400" frameBorder="0"></iframe>
