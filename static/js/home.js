3a864ae09f36b794e2bb66d81f7c9b3718c60a10
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2014-02-10 16:34:51 -0800

    [layouts] give feedback to user when not allow to vote.

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,18 @@
+var NB_VOTES_REACHED = "maximum-number-of-votes-reached";
+
 $(window).load(function() {
 	$('.vote-btn').on('click', function() {
 		var idPresentation = $(this).attr('data-presentation');
-		$.post("/vote/" + idPresentation, function() {});
+		$.post("/vote/" + idPresentation, function(data) {
+			console.log(data);
+		}).fail(function(data) {
+			if (data.responseText == NB_VOTES_REACHED) {
+				var $alertsContainer = $(".alert-container");
+				$alertsContainer.html("<div class=\"alert alert-warning alert-dismissable\">" +
+  "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>" +
+  "You have reached your number of votes, please deselect a presentation if you changed your mind!</div>");
+			}
+				
+		});
 	});
 });
